54f464be75c57fbd4e3b412110ae05d56310e84a
Yoshihide Chubachi <yoshi@chubachi.net>
2014-11-18 12:10:42 +0000

    Add one more test

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -3,7 +3,11 @@ require "mini_test_practice/version"
 module MiniTestPractice
   class MyClass
     def odd?(value)
-      return true
+      if value == 1 then
+        return true
+      else
+        return false
+      end
     end
   end
 end
